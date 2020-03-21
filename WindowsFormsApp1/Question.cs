3733898e96d4743a35f699bb1a66937d4b6afc9f
kalamanek <kalamanek@gmail.com>
2020-03-22 00:09:13 +0100

    Add project files.

---
+++ WindowsFormsApp1/Question.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WindowsFormsApp1
+{
+    public struct Question
+    {
+        private String question { get; }
+        private String[] answear { get; }
+        private int correctAnswear { get; }
+
+        public Question(string question, string[] answear, int correctAnswear)
+        {
+            this.question = question;
+            this.answear = answear;
+            this.correctAnswear = correctAnswear;
+        }
+        public int getPoints()
+        {
+            return 1;
+        }
+    }
+}
